32edd3289cb0ef391166e53a6f1211067d3556b6
Csaba19999 <menesi777@gmail.com>
2021-12-09 14:58:04 +0100

    task help nem tenyleges kommit

--- ujTask.js
+++ ujTask.js
@@ -1,17 +1,26 @@
-const myAjax = new MyAjax();
-let apiVegPont = "http://localhost:3000/TASK";
+$(function() {
 
-$("#ujAdat").on("click", () => {
-    let szerzo= $("#szerzo").val();
-    let cim= $("#cim").val();
-    let ar= $("#ar").val();
-    let tipus= $("#tipus").val();
+$("#feltolt").on("click", () => {
+    const myAjax = new MyAjax();
+    let apiVegPont = "http://localhost:3000/TASK";
+    let tasks = [];
+    let id = myAjax.getAdat(apiVegPont, tasks, );
+    let cim = $("#cim").val();
+    let leiras = $("#leiras").val();
+    let  = $("#status").val();
+    let hatarido = $("#hatarido").val();
+    let elvegzi = $("#elvegzi").val();
 
     let ujAdat = {
-      szerzo: szerzo,
-      cim: cim,
-      ar: ar,
-      tipus: tipus
+        id: id,
+        title: cim,
+        description: leiras,
+        endDate : hatarido,
+        userId : felhasznalo,
+        status : elvegzi
     };
     myAjax.postAdat(apivegpont, ujAdat);
+    console.log("Adat felvitel működik")
   });
+});
+
