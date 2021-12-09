59b150d3c89e3bb30f568ace15e0089631c4f802
BGabesz <bogdan.gabesz@gmail.com>
2021-12-09 14:35:16 +0100

    task.js

---
+++ task.js
@@ -0,0 +1,41 @@
+class Task{
+    constructor(elem, adat,id){
+        this.elem = elem;
+        this.cim = this.elem.children("h3");
+        this.leiras=this.elem.children("p");
+        this.endDate=this.endDate.children("p");
+        this.nev=this.nev.children("p");
+        this.status=this.status.status("p");
+        this.gomb=this.elem.children(".modosit");
+        this.gomb=this.elem.children(".torles");
+
+        this.gomb.on("click", () => {
+            this.kattintModosit();
+        });
+        this.gomb.on("click", () => {
+            this.kattintTorol();
+        });
+    }
+    
+        
+//        this.gomb.on("click", () => {
+//            this.kattint();
+//        });
+
+kattintModosit(){
+    let esemeny = new CustomEvent("modosit", {detail: this.adat});
+    window.dispatchEvent(esemeny);
+    console.log("modosit");
+}
+kattintTorles(){
+    let esemeny = new CustomEvent("torol", {detail: this.adat});
+    window.dispatchEvent(esemeny);
+    console.log("torol");
+}
+setAdatok(ertekek){
+    this.nev.html(ertekek.nev);
+    this.kep.attr("src", ertekek.kep);
+    this.leiras.html(ertekek.leiras);
+    this.ar.html(ertekek.ar);
+}
+}
